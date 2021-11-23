79fb709b9bff38af3e1da8fa2d06104d915d5c91
SwitchCaseKam <kamil.leczycki.ns@gmail.com>
2021-11-23 20:52:01 +0100

    [none] add appendChild, removeChild support

--- src/app/dom/renderer2/renderer2.component.ts
+++ src/app/dom/renderer2/renderer2.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-renderer2',
@@ -9,42 +9,36 @@ export class Renderer2Component implements OnInit, AfterViewInit {
 
   public formsIds = ['sq1', 'rect1', 'circ1'];
   public selectedFormId: number = 0;
-  @ViewChild('sq1') sq1: ElementRef = new ElementRef({});
+  @ViewChild('sq1' ) sq1: ElementRef = new ElementRef({});
   @ViewChild('rect1') rect1: ElementRef = new ElementRef({});
   @ViewChild('circ1') circ1: ElementRef = new ElementRef({});
+  @ViewChild('addClass') addClassButton: ElementRef = new ElementRef({});
+  @ViewChild('removeClass') removeClassButton: ElementRef = new ElementRef({});
+  @ViewChild('updateStyle') updateStyleButton: ElementRef = new ElementRef({});
+  @ViewChild('createElement') createElementButton: ElementRef = new ElementRef({});
+  @ViewChild('removeElement') removeElementButton: ElementRef = new ElementRef({});
   private childsComponents: ElementRef[] = [];
   
   constructor(private renderer2: Renderer2) { }
 
   public ngOnInit(): void {
+
   }
 
   public ngAfterViewInit(): void {
     this.childsComponents = [this.sq1, this.rect1, this.circ1];
+    this.renderer2.listen(this.addClassButton.nativeElement, 'click', () => this.addClass());
+    this.renderer2.listen(this.removeClassButton.nativeElement, 'click', () => this.removeClass());
+    this.renderer2.listen(this.updateStyleButton.nativeElement, 'click', () => this.updateDOM());
+    this.renderer2.listen(this.createElementButton.nativeElement, 'click', () => this.createNewElement());
+    this.renderer2.listen(this.removeElementButton.nativeElement, 'click', () => this.removeElement());
   }
 
-  public updateDOM(): void {
+  private updateDOM(): void {
     const form = this.childsComponents[this.selectedFormId];
     this.renderer2.setProperty(form.nativeElement, 'innerHTML', this.generateNumber());
     this.renderer2.setStyle(form.nativeElement, 'background-color', this.generateColor());
     this.renderer2.setStyle(form.nativeElement, 'border', this.generateBorder());
-  }
-
-  public removeClass(): void {
-    const form = this.childsComponents[this.selectedFormId];
-    this.renderer2.removeClass(form.nativeElement, 'form');
-
-  }
-
-  public addClass(): void {
-    const form = this.childsComponents[this.selectedFormId];
-    this.renderer2.addClass(form.nativeElement, 'new-form-class');
-  }
-
-  public appendChild(): void {
-    const form = this.childsComponents[this.selectedFormId];
-    this.renderer2.appendChild(form.nativeElement, 'new-form-class');
-
   }
 
   private generateNumber(): string {
@@ -63,5 +57,30 @@ export class Renderer2Component implements OnInit, AfterViewInit {
     const borderStyles = ['dotted', 'dashed', 'solid', 'double', 'ridge'];
     const index = Math.round(Math.random()*10)%borderStyles.length;
     return `${borderSize} ${color} ${borderStyles[index]}`; 
+  }
+
+  private removeClass(): void {
+    const form = this.childsComponents[this.selectedFormId];
+    this.renderer2.removeClass(form.nativeElement, 'form');
+  }
+
+  private addClass(): void {
+    const form = this.childsComponents[this.selectedFormId];
+    this.renderer2.addClass(form.nativeElement, 'new-form-class');
+  }
+
+  private createNewElement(): void {
+    const newElement = this.renderer2.createElement('div');
+    this.renderer2.setProperty(newElement, 'innerHTML', 'new div element created by renderer2');
+    this.renderer2.setStyle(newElement, 'border', '2px solid blue');
+    this.renderer2.setStyle(newElement, 'background-color', 'white');
+    this.renderer2.addClass(newElement, 'new-element-class');
+    const form = this.childsComponents[this.selectedFormId];
+    this.renderer2.appendChild(form.nativeElement, newElement);
+  }
+
+  private removeElement(): void {
+    const form = this.childsComponents[this.selectedFormId];
+    this.renderer2.removeChild(form.nativeElement,document.querySelector('.new-element-class'));
   }
 }
