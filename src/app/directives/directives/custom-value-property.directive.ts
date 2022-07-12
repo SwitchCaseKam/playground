9ee617a8abeb8d3522f222fc04d880d5292f45b4
SwitchCaseKam <kamil.leczycki.ns@gmail.com>
2022-07-12 18:27:22 +0200

    [directives] add customValuePropoerty attribute directive

--- src/app/directives/directives/custom-value-property.directive.ts
+++ src/app/directives/directives/custom-value-property.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, ElementRef, Input, OnInit, Renderer2, TemplateRef, ViewContainerRef } from '@angular/core';
+import { style } from '@angular/animations';
+import { Directive, ElementRef, HostBinding, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[customValueProperty]'
@@ -7,40 +8,30 @@ export class CustomValuePropertyDirective implements OnInit {
 
   @Input()
   delay = 0;
+
+  @Input()
+  @HostBinding('innerHTML')
+  customText = 'default';
   
   @Input()
-  color = '';
+  @HostBinding('style.backgroundColor')
+  primaryColor = '';
 
-
+  @Input()
+  secondaryColor = '';
 
   constructor(
     private elementRef: ElementRef,
     private renderer2: Renderer2,
-    private viewContainerRef: ViewContainerRef,
-    private templateRef: TemplateRef<any>
-  ) {
-    // console.log('el.ref = ', elementRef.nativeElement);
-    // const currentElement = elementRef.nativeElement;
-    // console.log('value = ', this.delay);
-
-    // setTimeout(() => {
-    //   if (currentElement) {
-    //     this.renderer2.setStyle(currentElement, 'background-color', 'green');
-    //   }
-    // }, this.delay);
-  }
+  ) { }
 
   public ngOnInit(): void {
-    this.viewContainerRef.createEmbeddedView(this.templateRef);
     const currentElement = this.elementRef.nativeElement;
-    console.log('el.ref = ', this.elementRef.nativeElement);
-    console.log('delay = ', this.delay);
-    console.log('color = ', this.color);
-    // setTimeout(() => {
-    //   if (currentElement) {
-    //     // this.renderer2.setStyle(currentElement, 'background-color', 'green');
-    //   }
-    // }, this.delay);
+    setTimeout(() => {
+      this.renderer2.setStyle(currentElement, 'background-color', this.secondaryColor);
+      this.renderer2.setStyle(currentElement, 'color', 'white');
+    }, this.delay)
+
   }
 
 }
